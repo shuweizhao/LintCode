b9a49329b7c626e3937adff813ad7d66a683546d
szhao8 <szhao8@ncsu.edu>
2015-06-20 16:11:48 -0400

    Add LintCode using O(1) to check power of 2

---
+++ checkPowerOf2.java
@@ -0,0 +1,30 @@
+class Solution {
+    /*
+     * @param n: An integer
+     * @return: True or false
+     */
+		
+	//version 1 
+    public boolean checkPowerOf2(int n) {
+        // write your code here
+        if (n <= 0) {
+            return false;
+        }
+        if ((n & -n) == n) {
+            return true;
+        } 
+        return false;
+    }
+	//version 2
+	/*
+	public boolean checkPowerOf2(int n) {
+        // write your code here
+        if (n <= 0) {
+            return false;
+        }
+        if ((n & n - 1) == 0) {
+            return true;
+        } 
+        return false;
+    }*/
+};
