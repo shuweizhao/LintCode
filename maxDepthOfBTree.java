5cac527f296bb7460881ad513001f6d45275a0b0
szhao8 <szhao8@ncsu.edu>
2015-06-24 10:51:24 -0400

    Easy, Divide and Conquer, Recursion, Binary Tree

---
+++ maxDepthOfBTree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
+    public int maxDepth(TreeNode root) {
+        // write your code here
+        int depth = 0;
+        if (root == null) return 0;
+        return getMax(root);
+        //return depth;
+    }
+    
+    public int getMax(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return Math.max(getMax(root.left) , getMax(root.right)) + 1;
+    }
+  }
