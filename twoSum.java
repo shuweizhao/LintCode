1a5f46412ab95f235b34ef7f7854ab07b8fc63cd
szhao8 <szhao8@ncsu.edu>
2015-06-27 23:33:00 -0400

    Medium, Two Pointers, Sort, Array

---
+++ twoSum.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /*
+     * @param numbers : An array of Integer
+     * @param target : target = numbers[index1] + numbers[index2]
+     * @return : [index1 + 1, index2 + 1] (index1 < index2)
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        // write your code here
+        int[] result = new int[2];
+        if (numbers == null || numbers.length == 0) {
+            return result;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] == target - numbers[i]) {
+                    result[0] = i + 1;
+                    result[1] = j + 1;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
